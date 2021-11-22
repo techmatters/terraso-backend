14d7fb64d79d77756f641b1a77ed1290510c0fa8
Caio Carrara <caio@techmatters.org>
2021-11-22 15:20:50 -0300

    fix: adds end slash to GraphQL endpoint

--- terraso_backend/config/urls.py
+++ terraso_backend/config/urls.py
@@ -5,5 +5,5 @@ from graphene_django.views import GraphQLView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True))),
+    path("graphql/", csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
