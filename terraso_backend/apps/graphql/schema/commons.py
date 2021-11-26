12029f1ec5327b942a1c0f4ae421381135b07518
Caio Carrara <cacarrara@users.noreply.github.com>
2021-11-26 15:37:56 -0300

    feat: add base GraphQL write and delete mutations (#14)
    This change adds generic base mutations to be used (specialized) by
    other more specialized (models) mutations.

---
+++ terraso_backend/apps/graphql/schema/commons.py
@@ -0,0 +1,50 @@
+import graphql_relay
+from graphene import relay
+
+
+class BaseWriteMutation(relay.ClientIDMutation):
+    model_class = None
+
+    @classmethod
+    def mutate_and_get_payload(cls, root, info, **kwargs):
+        """
+        This is the method performed everytime this mutation is submitted.
+        Since this is the base class for write operations, this method will be
+        called both when adding and updating a model. The `kwargs` receives
+        a dictionary with all inputs informed.
+        """
+        graphql_id = kwargs.pop("id", None)
+
+        if graphql_id:
+            _, _pk = graphql_relay.from_global_id(graphql_id)
+            model_instance = cls.model_class.objects.get(pk=_pk)
+        else:
+            model_instance = cls.model_class()
+
+        for attr, value in kwargs.items():
+            setattr(model_instance, attr, value)
+
+        model_instance.save()
+
+        result_kwargs = {cls.model_class.__name__.lower(): model_instance}
+
+        return cls(**result_kwargs)
+
+
+class BaseDeleteMutation(relay.ClientIDMutation):
+    model_class = None
+
+    @classmethod
+    def mutate_and_get_payload(cls, root, info, **kwargs):
+        graphql_id = kwargs.pop("id", None)
+
+        if not graphql_id:
+            model_instance = None
+        else:
+            _, _pk = graphql_relay.from_global_id(graphql_id)
+            model_instance = cls.model_class.objects.get(pk=_pk)
+            model_instance.delete()
+
+        result_kwargs = {cls.model_class.__name__.lower(): model_instance}
+
+        return cls(**result_kwargs)
