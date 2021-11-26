86cadbcbfc47a14a12e035b29be0b5b2438d5d4b
Caio Carrara <cacarrara@users.noreply.github.com>
2021-11-26 18:28:21 -0300

    feat: add User mutation to the GraphQL API (#16)
    This change adds the mutations (add, update, delete) to the User
    resource on GraphQL API. The operations are still a bit naive and can be
    improved in matters of validation, mainly. Anyway, this already looks
    like a good increment to the project.

--- terraso_backend/apps/graphql/schema/__init__.py
+++ terraso_backend/apps/graphql/schema/__init__.py
@@ -14,7 +14,7 @@ from .landscapes import (
     LandscapeNode,
     LandscapeUpdateMutation,
 )
-from .users import UserNode
+from .users import UserAddMutation, UserDeleteMutation, UserNode, UserUpdateMutation
 
 
 class Query(graphene.ObjectType):
@@ -29,10 +29,13 @@ class Query(graphene.ObjectType):
 class Mutations(graphene.ObjectType):
     add_group = GroupAddMutation.Field()
     add_landscape = LandscapeAddMutation.Field()
+    add_user = UserAddMutation.Field()
     update_group = GroupUpdateMutation.Field()
     update_landscape = LandscapeUpdateMutation.Field()
+    update_user = UserUpdateMutation.Field()
     delete_group = GroupDeleteMutation.Field()
     delete_landscape = LandscapeDeleteMutation.Field()
+    delete_user = UserDeleteMutation.Field()
 
 
 schema = graphene.Schema(query=Query, mutation=Mutations)
