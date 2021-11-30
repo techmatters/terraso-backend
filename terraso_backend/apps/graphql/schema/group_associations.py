e3d1bcac8438486a18146222e119a8de0ad2ae2a
Caio Carrara <caio@techmatters.org>
2021-11-30 11:38:41 -0300

    chore: standardize usage of lists and tuples on schema

--- terraso_backend/apps/graphql/schema/group_associations.py
+++ terraso_backend/apps/graphql/schema/group_associations.py
@@ -7,6 +7,6 @@ from apps.core.models import GroupAssociation
 class GroupAssociationNode(DjangoObjectType):
     class Meta:
         model = GroupAssociation
-        filter_fields = ["parent_group", "child_group"]
-        fields = ["parent_group", "child_group"]
+        filter_fields = ("parent_group", "child_group")
+        fields = ("parent_group", "child_group")
         interfaces = (relay.Node,)
