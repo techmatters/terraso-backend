d3fcc139c94d17d82542fe6b39dac7b9d8335757
Caio Carrara <cacarrara@users.noreply.github.com>
2021-12-02 13:39:01 -0300

    feat: add GroupAssociation and Membership mutations (#32)
    This change adds the mutation to add and to delete GroupAssociations
    through GraphQL API. It doesn't make sense to update a GroupAssociation,
    so this mutation wasn't added.
    
    This change also adds the mutations to Membership through GraphQL API.

--- terraso_backend/apps/graphql/schema/__init__.py
+++ terraso_backend/apps/graphql/schema/__init__.py
@@ -2,7 +2,11 @@ import graphene
 from graphene import relay
 from graphene_django.filter import DjangoFilterConnectionField
 
-from .group_associations import GroupAssociationNode
+from .group_associations import (
+    GroupAssociationAddMutation,
+    GroupAssociationDeleteMutation,
+    GroupAssociationNode,
+)
 from .groups import (
     GroupAddMutation,
     GroupDeleteMutation,
@@ -21,7 +25,12 @@ from .landscapes import (
     LandscapeNode,
     LandscapeUpdateMutation,
 )
-from .memberships import MembershipNode
+from .memberships import (
+    MembershipAddMutation,
+    MembershipDeleteMutation,
+    MembershipNode,
+    MembershipUpdateMutation,
+)
 from .users import UserAddMutation, UserDeleteMutation, UserNode, UserUpdateMutation
 
 
@@ -47,14 +56,19 @@ class Mutations(graphene.ObjectType):
     add_landscape = LandscapeAddMutation.Field()
     add_user = UserAddMutation.Field()
     add_landscape_group = LandscapeGroupAddMutation.Field()
+    add_group_association = GroupAssociationAddMutation.Field()
+    add_membership = MembershipAddMutation.Field()
     update_group = GroupUpdateMutation.Field()
     update_landscape = LandscapeUpdateMutation.Field()
     update_landscape_group = LandscapeGroupUpdateMutation.Field()
+    update_membership = MembershipUpdateMutation.Field()
     update_user = UserUpdateMutation.Field()
     delete_group = GroupDeleteMutation.Field()
     delete_landscape = LandscapeDeleteMutation.Field()
     delete_user = UserDeleteMutation.Field()
     delete_landscape_group = LandscapeGroupDeleteMutation.Field()
+    delete_group_association = GroupAssociationDeleteMutation.Field()
+    delete_membership = MembershipDeleteMutation.Field()
 
 
 schema = graphene.Schema(query=Query, mutation=Mutations)
---
+++ terraso_backend/tests/graphql/mutations/test_membership_mutations.py
@@ -0,0 +1,164 @@
+import pytest
+
+from apps.core.models import Membership
+
+pytestmark = pytest.mark.django_db
+
+
+def test_membership_add(client_query, groups, users):
+    group = groups[0]
+    user = users[0]
+
+    response = client_query(
+        """
+        mutation addMembership($input: MembershipAddMutationInput!){
+          addMembership(input: $input) {
+            membership {
+              id
+              userRole
+              user {
+                email
+              }
+              group {
+                slug
+              }
+            }
+          }
+        }
+        """,
+        variables={
+            "input": {
+                "userEmail": user.email,
+                "groupSlug": group.slug,
+            }
+        },
+    )
+    membership = response.json()["data"]["addMembership"]["membership"]
+
+    assert membership["id"]
+    assert membership["user"]["email"] == user.email
+    assert membership["group"]["slug"] == group.slug
+    assert membership["userRole"] == Membership.ROLE_MEMBER.upper()
+
+
+def test_membership_add_manager(client_query, groups, users):
+    group = groups[0]
+    user = users[0]
+
+    response = client_query(
+        """
+        mutation addMembership($input: MembershipAddMutationInput!){
+          addMembership(input: $input) {
+            membership {
+              id
+              userRole
+              user {
+                email
+              }
+              group {
+                slug
+              }
+            }
+          }
+        }
+        """,
+        variables={
+            "input": {
+                "userEmail": user.email,
+                "groupSlug": group.slug,
+                "userRole": Membership.ROLE_MANAGER,
+            }
+        },
+    )
+    membership = response.json()["data"]["addMembership"]["membership"]
+
+    assert membership["id"]
+    assert membership["user"]["email"] == user.email
+    assert membership["group"]["slug"] == group.slug
+    assert membership["userRole"] == Membership.ROLE_MANAGER.upper()
+
+
+def test_membership_update(client_query, memberships):
+    old_membership = _get_memberships(client_query)[0]
+
+    assert old_membership["userRole"] != Membership.ROLE_MANAGER.upper()
+
+    response = client_query(
+        """
+        mutation updateMembership($input: MembershipUpdateMutationInput!){
+          updateMembership(input: $input) {
+            membership {
+              id
+              userRole
+              user {
+                email
+              }
+              group {
+                slug
+              }
+            }
+          }
+        }
+        """,
+        variables={
+            "input": {
+                "id": old_membership["id"],
+                "userRole": Membership.ROLE_MANAGER,
+            }
+        },
+    )
+    membership = response.json()["data"]["updateMembership"]["membership"]
+
+    assert membership["id"]
+    assert membership["user"]["email"] == old_membership["user"]["email"]
+    assert membership["group"]["slug"] == old_membership["group"]["slug"]
+    assert membership["userRole"] == Membership.ROLE_MANAGER.upper()
+
+
+def test_membership_delete(client_query, memberships):
+    old_membership = _get_memberships(client_query)[0]
+
+    response = client_query(
+        """
+        mutation deleteMembership($input: MembershipDeleteMutationInput!){
+          deleteMembership(input: $input) {
+            membership {
+              id
+            }
+          }
+        }
+        """,
+        variables={
+            "input": {
+                "id": old_membership["id"],
+            }
+        },
+    )
+    membership = response.json()["data"]["deleteMembership"]["membership"]
+
+    assert membership["id"]
+    assert not Membership.objects.filter(
+        user__email=old_membership["user"]["email"],
+        group__slug=old_membership["group"]["slug"],
+    )
+
+
+def _get_memberships(client_query):
+    response = client_query(
+        """
+        {
+          memberships {
+            edges {
+              node {
+                id
+                userRole
+                user { email }
+                group { slug }
+              }
+            }
+          }
+        }
+        """
+    )
+    edges = response.json()["data"]["memberships"]["edges"]
+    return [e["node"] for e in edges]
