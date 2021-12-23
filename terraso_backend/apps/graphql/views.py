5052baf65f2674413a62856c91f0711f3313b49c
Caio Carrara <caio@techmatters.org>
2021-12-22 22:09:00 -0300

    chore: deactivate API authentication until clients ready

--- terraso_backend/apps/graphql/views.py
+++ terraso_backend/apps/graphql/views.py
@@ -1,12 +1,12 @@
-from django.http import JsonResponse
 from graphene_django.views import GraphQLView
 
 
 class TerrasoGraphQLView(GraphQLView):
     def dispatch(self, request, *args, **kwargs):
-        if not request.user.is_authenticated:
-            return JsonResponse(
-                {"error": "Permission denied"}, status=403
-            )
+        # TODO: uncomment following code when client ready for authentication
+        #  if not request.user.is_authenticated:
+        #      return JsonResponse(
+        #          {"error": "Permission denied"}, status=403
+        #      )
 
         return super().dispatch(request, *args, **kwargs)
