83c37abc41eab3c77847fa59fa8a2dccbcd24af6
Caio Carrara <caio@techmatters.org>
2022-01-19 17:29:49 -0300

    chore: update migration name

--- terraso_backend/apps/core/migrations/0007_auto_20220118_2227.py
+++ terraso_backend/apps/core/migrations/0007_auto_20220118_2227.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("core", "0005_auto_20220113_1747"),
+        ("core", "0006_alter_group_options"),
     ]
 
     operations = [
