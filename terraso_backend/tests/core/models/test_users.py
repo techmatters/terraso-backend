4b62813f3371a9f71a7ae2262bbb8b3025a739aa
Caio Carrara <caio@techmatters.org>
2022-01-21 15:30:06 -0300

    feat: add method to check if user is Landscape manager

--- terraso_backend/tests/core/models/test_users.py
+++ terraso_backend/tests/core/models/test_users.py
@@ -1,7 +1,7 @@
 import pytest
 from mixer.backend.django import mixer
 
-from apps.core.models.users import User
+from apps.core.models import Group, Landscape, LandscapeGroup, User
 
 pytestmark = pytest.mark.django_db
 
@@ -11,3 +11,25 @@ def test_user_string_format_is_its_email():
     user = mixer.blend(User, email=user_email)
 
     assert user_email == str(user)
+
+
+@pytest.mark.parametrize(
+    "is_default_landscape_group, is_expected_to_be_manager",
+    (
+        (True, True),
+        (False, False),
+    ),
+)
+def test_user_is_landscape_manager(is_default_landscape_group, is_expected_to_be_manager):
+    user = mixer.blend(User)
+    group = mixer.blend(Group)
+    landscape = mixer.blend(Landscape)
+    mixer.blend(
+        LandscapeGroup,
+        landscape=landscape,
+        group=group,
+        is_default_landscape_group=is_default_landscape_group,
+    )
+    group.add_manager(user)
+
+    assert user.is_landscape_manager(landscape.id) is is_expected_to_be_manager
