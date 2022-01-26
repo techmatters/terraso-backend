bf4fd928e7342b0a9d219bc5b1cd9cd1e9106bf9
Caio Carrara <cacarrara@users.noreply.github.com>
2022-01-26 14:41:32 -0300

    feat: removes LandscapeGroupUpdateMutation (#106)
    Landscape Group (the relationship model between Landscape and Group)
    isn't expected to be updated. The only attribute this entity has is the
    `is_default_landscape_group` that is more a internal attribute and won't
    be updated.

--- terraso_backend/apps/graphql/schema/landscape_groups.py
+++ terraso_backend/apps/graphql/schema/landscape_groups.py
@@ -26,8 +26,12 @@ class LandscapeGroupNode(DjangoObjectType):
         connection_class = TerrasoConnection
 
 
-class LandscapeGroupWriteMutation(relay.ClientIDMutation):
+class LandscapeGroupAddMutation(relay.ClientIDMutation):
     landscape_group = graphene.Field(LandscapeGroupNode)
+
+    class Input:
+        landscape_slug = graphene.String(required=True)
+        group_slug = graphene.String(required=True)
 
     @classmethod
     def mutate_and_get_payload(cls, root, info, **kwargs):
@@ -37,27 +41,9 @@ class LandscapeGroupWriteMutation(relay.ClientIDMutation):
         called both when adding and updating Landscape Groups. The `kwargs`
         receives a dictionary with all inputs informed.
         """
-        _id = kwargs.pop("id", None)
-
-        if _id:
-            landscape_group = LandscapeGroup.objects.get(pk=_id)
-            new_landscape = kwargs.pop("landscape_slug", None)
-            new_group = kwargs.pop("group_slug", None)
-
-            if new_landscape:
-                landscape_group.landscape = Landscape.objects.get(slug=new_landscape)
-
-            if new_group:
-                landscape_group.group = Group.objects.get(slug=new_group)
-        else:
-            landscape_group = LandscapeGroup()
-            landscape_group.is_dafault_landscape_group = False
-            landscape_group.landscape = Landscape.objects.get(slug=kwargs.pop("landscape_slug"))
-            landscape_group.group = Group.objects.get(slug=kwargs.pop("group_slug"))
-
-        default_group = kwargs.pop("is_default_landscape_group", None)
-        if default_group is not None:
-            landscape_group.is_default_landscape_group = default_group
+        landscape_group = LandscapeGroup()
+        landscape_group.landscape = Landscape.objects.get(slug=kwargs.pop("landscape_slug"))
+        landscape_group.group = Group.objects.get(slug=kwargs.pop("group_slug"))
 
         try:
             landscape_group.full_clean()
@@ -67,21 +53,6 @@ class LandscapeGroupWriteMutation(relay.ClientIDMutation):
         landscape_group.save()
 
         return cls(landscape_group=landscape_group)
-
-
-class LandscapeGroupAddMutation(LandscapeGroupWriteMutation):
-    class Input:
-        landscape_slug = graphene.String(required=True)
-        group_slug = graphene.String(required=True)
-        is_default_landscape_group = graphene.Boolean(required=False, default=False)
-
-
-class LandscapeGroupUpdateMutation(LandscapeGroupWriteMutation):
-    class Input:
-        id = graphene.ID(required=True)
-        landscape_slug = graphene.String()
-        group_slug = graphene.String()
-        is_default_landscape_group = graphene.Boolean(required=False, default=False)
 
 
 class LandscapeGroupDeleteMutation(BaseDeleteMutation):
