baab5915f75c7e6d3e05c9b0de8cf19b09eb745a
Caio Carrara <caio@techmatters.org>
2022-01-27 22:20:54 -0300

    feat: add more specific GraphQL exceptions
    This change adds more specific GraphQL exceptions and properly handle
    the error messages to expose error codes instead of error messages.

--- terraso_backend/apps/graphql/exceptions.py
+++ terraso_backend/apps/graphql/exceptions.py
@@ -5,8 +5,24 @@ class GraphQLValidationException(Exception):
 
         for field, validation_errors in validation_error.error_dict.items():
             for error in validation_errors:
-                messages.extend([f"field={field}, error={message}" for message in error.messages])
+                messages.append(_format_error_message(field, error.code))
 
-        error_message = ";".join(messages)
+        error_message = "; ".join(messages)
 
         return cls(error_message)
+
+
+class GraphQLNotFoundException(GraphQLValidationException):
+    def __init__(self, message="", field=None):
+        super().__init__(_format_error_message(field, "not_found", extra=message))
+
+
+class GraphQLNotAllowedException(GraphQLValidationException):
+    def __init__(self, message="", field=None, operation="operation"):
+        super().__init__(_format_error_message(field, f"{operation}_not_allowed", extra=message))
+
+
+def _format_error_message(field, error_code, extra=""):
+    if extra:
+        return f"field={field}, error_code={error_code}, message={extra}"
+    return f"field={field}, error_code={error_code}"
