7233944522fbb6337d572abdbcce1258d06a1a63
Caio Carrara <caio@techmatters.org>
2022-04-07 17:31:31 -0300

    feat: adds OAuth provider capability
    This change makes use of `django-oauth-toolkit`[1] library to add the
    capability for Terraso be an OAuth provider. The few settings added are
    the minimum to make it work.
    
    [1] - https://django-oauth-toolkit.readthedocs.io/en/latest/

--- terraso_backend/config/urls.py
+++ terraso_backend/config/urls.py
@@ -3,7 +3,8 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("graphql/", include("apps.graphql.urls", namespace="terraso_graphql")),
     path("auth/", include("apps.auth.urls", namespace="terraso_auth")),
+    path("graphql/", include("apps.graphql.urls", namespace="terraso_graphql")),
     path("storage/", include("apps.storage.urls", namespace="terraso_storage")),
+    path("oauth/", include("oauth2_provider.urls", namespace="oauth2_provider")),
 ]
