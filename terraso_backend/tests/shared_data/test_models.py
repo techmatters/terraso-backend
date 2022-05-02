4ba6db957dbaf896989adf470690e3225cf57279
Caio Carrara <cacarrara@users.noreply.github.com>
2022-05-02 15:07:49 -0300

    feat: create the model layer of shared data app (#156)
    * chore: expose SlugModel from core app
    
    This change only expose the SlugModel from core app so other apps can
    have models based on SlugModel.
    
    * feat: create the model layer of shared data app
    
    This change adds the model layer for the new `shared_data` app. The app
    will be the central place for the "Shared Data Files" feature.
    
    * chore: adds permission tests for Data Entry model
    
    Adds permission tests for Data Entry model.
    
    * feat: add permission check to view data entry

---
+++ terraso_backend/tests/shared_data/test_models.py
@@ -0,0 +1,61 @@
+import pytest
+from mixer.backend.django import mixer
+
+from apps.shared_data.models import DataEntry
+
+pytestmark = pytest.mark.django_db
+
+
+def test_data_entry_string_format_is_its_name():
+    data_entry_name = "Test Survey Questions"
+    data_entry = mixer.blend(DataEntry, name=data_entry_name)
+
+    assert data_entry_name == str(data_entry)
+
+
+def test_data_entry_is_slugifiable_by_name():
+    data_entry = mixer.blend(DataEntry, name="Test Survey Data", slug=None)
+
+    assert data_entry.slug == "test-survey-data"
+
+
+def test_data_entry_can_be_updated_by_its_creator(user):
+    users_data_entry = mixer.blend(DataEntry, created_by=user)
+
+    assert user.has_perm(DataEntry.get_perm("change"), obj=users_data_entry)
+
+
+def test_data_entry_cannot_be_updated_by_non_creator(user, user_b):
+    any_data_entry = mixer.blend(DataEntry, created_by=user_b)
+
+    assert not user.has_perm(DataEntry.get_perm("change"), obj=any_data_entry)
+
+
+def test_data_entry_can_be_deleted_by_its_creator(user):
+    users_data_entry = mixer.blend(DataEntry, created_by=user)
+
+    assert user.has_perm(DataEntry.get_perm("delete"), obj=users_data_entry)
+
+
+def test_data_entry_cannot_be_deleted_by_non_creator(user, user_b):
+    any_data_entry = mixer.blend(DataEntry, created_by=user_b)
+
+    assert not user.has_perm(DataEntry.get_perm("delete"), obj=any_data_entry)
+
+
+def test_data_entry_can_be_viewed_by_group_members(user, user_b, group):
+    group.members.add(user, user_b)
+
+    group_data_entry = mixer.blend(DataEntry, created_by=user)
+    group_data_entry.groups.add(group)
+
+    assert user_b.has_perm(DataEntry.get_perm("view"), obj=group_data_entry)
+
+
+def test_data_entry_cannot_be_viewed_by_non_group_members(user, user_b, group):
+    group.members.add(user)
+
+    group_data_entry = mixer.blend(DataEntry, created_by=user)
+    group_data_entry.groups.add(group)
+
+    assert not user_b.has_perm(DataEntry.get_perm("view"), obj=group_data_entry)
