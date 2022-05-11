dea07c0a4e105830535fd5dbac59fd0c23ea5cb7
Caio Carrara <caio@techmatters.org>
2022-05-11 10:54:35 -0300

    feat: update base upload service
    This change updates the base upload service. It makes upload method
    accept a file name, and in case it's not informed a unique name is
    created. It also adds the feature to upload file with the same name
    adding a counter as the name suffix.

--- terraso_backend/apps/storage/services.py
+++ terraso_backend/apps/storage/services.py
@@ -1,4 +1,6 @@
+import pathlib
 import urllib.request
+import uuid
 
 from django.conf import settings
 from django.core.files.base import ContentFile
@@ -19,25 +21,46 @@ class UploadService:
         file = self._download_from_url(url)
         return self.upload_file(user_id, ContentFile(file))
 
-    def upload_file(self, user_id, file):
-        path = self.get_path_on_storage(user_id)
+    def upload_file(self, user_id, file, file_name=None):
+        if not file_name:
+            file_name = uuid.uuid4().hex
+
+        path = self.get_path_on_storage(user_id, file_name)
+
+        if self.storage.exists(path):
+            path = self._uniquify(path)
+
         self.storage.save(path, file)
         return self.get_uploaded_file_url(path)
 
-    def get_path_on_storage(self, user_id):
-        return f"{user_id}/"
+    def get_path_on_storage(self, user_id, file_name):
+        return f"{user_id}/{file_name}"
 
     def get_uploaded_file_url(self, path):
-        return "{}/{}".format(self.base_url, path)
+        return f"{self.base_url}/{path}"
 
     def _download_from_url(self, url):
         with urllib.request.urlopen(url) as file:
             return file.read()
+
+    def _uniquify(self, path):
+        path_object = pathlib.Path(path).resolve()
+        directory = path_object.home()
+        file_name = path_object.name
+        file_extension = path_object.suffix
+
+        counter = 1
+
+        given_path = directory.joinpath(f"{file_name}_{counter}{file_extension}")
+        while self.storage.exists(given_path):
+            counter += 1
+
+        return given_path
 
 
 class ProfileImageService(UploadService):
     storage = ProfileImageStorage()
     base_url = settings.PROFILE_IMAGES_BASE_URL
 
-    def get_path_on_storage(self, user_id):
-        return "{}/profile-image".format(user_id)
+    def get_path_on_storage(self, user_id, file_name):
+        return f"{user_id}/profile-image"
