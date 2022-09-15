5e268d517ec52951f4ea365d06a6418efc543f9e
Jose Buitron <jose.buitron@gmail.com>
2022-09-15 13:54:05 -0500

    feat: Added visualization config to admin panel

--- terraso_backend/apps/shared_data/admin.py
+++ terraso_backend/apps/shared_data/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from .models import DataEntry
+from .models import DataEntry, VisualizationConfig
 
 
 @admin.register(DataEntry)
 class DataEntryAdmin(admin.ModelAdmin):
     list_display = ("name", "url", "resource_type", "created_by")
+
+
+@admin.register(VisualizationConfig)
+class VisualizationConfigAdmin(admin.ModelAdmin):
+    list_display = ("id", "data_entry", "configuration", "created_by", "created_at")
