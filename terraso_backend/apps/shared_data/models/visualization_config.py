a6e92db63938c773d6fb094348cedf700fbe648b
Jose Buitron <jose.buitron@gmail.com>
2022-09-27 14:44:08 -0500

    feat: Added title, slug and group to visualization config

--- terraso_backend/apps/shared_data/models/visualization_config.py
+++ terraso_backend/apps/shared_data/models/visualization_config.py
@@ -1,12 +1,14 @@
 from django.db import models
 
-from apps.core.models import BaseModel, User
+from apps.core.models import BaseModel, Group, SlugModel, User
+from apps.core.models.commons import validate_name
 from apps.shared_data import permission_rules as perm_rules
 
 from .data_entries import DataEntry
 
 
-class VisualizationConfig(BaseModel):
+class VisualizationConfig(SlugModel):
+    title = models.CharField(max_length=128, validators=[validate_name])
     configuration = models.JSONField(blank=True, null=True)
     created_by = models.ForeignKey(
         User,
@@ -18,8 +20,18 @@ class VisualizationConfig(BaseModel):
     data_entry = models.ForeignKey(
         DataEntry, on_delete=models.CASCADE, related_name="visualizations"
     )
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name="visualizations")
+
+    field_to_slug = "title"
 
     class Meta(BaseModel.Meta):
+        constraints = (
+            models.UniqueConstraint(
+                fields=("group_id", "slug"),
+                condition=models.Q(deleted_at__isnull=True),
+                name="shared_data_visualizationconfig_unique_active_slug_by_group",
+            ),
+        )
         verbose_name_plural = "Visualization Configs"
         rules_permissions = {
             "change": perm_rules.allowed_to_change_visualization_config,
