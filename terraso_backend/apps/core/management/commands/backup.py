7f53fbf3f9ed2144607d6c4e0ab2dde78c04d42e
david <david@techmatters.org>
2022-10-24 10:08:51 -0400

    feat: Add option to upload backup file to S3 bucket

--- terraso_backend/apps/core/management/commands/backup.py
+++ terraso_backend/apps/core/management/commands/backup.py
@@ -2,16 +2,23 @@ import json
 import os
 import traceback
 from datetime import datetime
+from pathlib import Path
+from tempfile import TemporaryDirectory
 
 from django.core import management
 from django.core.management.base import BaseCommand
 from django.db import connection
+
+from ._backup_storage import S3BackupStorage
 
 
 class Command(BaseCommand):
     """Backs up the application database to JSON files."""
 
     help = "Back up database to JSON files"
+
+    def add_arguments(self, parser):
+        parser.add_argument("--s3", action="store_true", help="Store backup in S3 bucket")
 
     @staticmethod
     def _query_migration_versions():
@@ -40,14 +47,37 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         """Command "main method"."""
         data_file, migration_file = self._generate_filenames()
+        if options["s3"]:
+            # results will be written to a temporary directory
+            tempdir = TemporaryDirectory()
+            tempdir_path = Path(tempdir.name)
+            data_file_name = data_file
+            migration_file_name = migration_file
+            data_file = str(tempdir_path / data_file)
+            migration_file = str(tempdir_path / migration_file)
+
+        def cleanup():
+            for filepath in data_file, migration_file:
+                if os.path.exists(filepath):
+                    os.unlink(filepath)
 
         try:
             management.call_command("dumpdata", output=data_file, verbosity=0, natural_foreign=True)
 
             migrations = self._query_migration_versions()
             self._save_migration_versions(migration_file, migrations)
+
+            if options["s3"]:
+                storage = S3BackupStorage()
+                with open(data_file, "rb") as data_fp, open(migration_file, "rb") as migration_fp:
+                    storage.save(data_file_name, data_fp)
+                    storage.save(migration_file_name, migration_fp)
+
         except Exception:
             traceback.print_exc()
-            for filepath in data_file, migration_file:
-                if os.path.exists(filepath):
-                    os.unlink(filepath)
+        finally:
+            if tempdir:
+                tempdir.cleanup()
+            if options["s3"]:
+                # if we are uploading the backups, we don't want extra files
+                cleanup()
