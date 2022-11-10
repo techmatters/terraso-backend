bd5014c3afa9438ebb883ed9f62a36b748cd1c6a
Jose Buitron <jose.buitron@gmail.com>
2022-11-10 14:42:42 -0500

    chore: Added test

--- terraso_backend/tests/storage/conftest.py
+++ terraso_backend/tests/storage/conftest.py
@@ -2,7 +2,7 @@ import pytest
 from mixer.backend.django import mixer
 
 from apps.auth.services import JWTService
-from apps.core.models import User
+from apps.core.models import Group, Landscape, LandscapeGroup, User
 
 
 @pytest.fixture
@@ -13,3 +13,12 @@ def access_token(user):
 @pytest.fixture
 def user():
     return mixer.blend(User)
+
+
+@pytest.fixture
+def landscape(user):
+    landscape = mixer.blend(Landscape)
+    group = mixer.blend(Group)
+    group.add_manager(user)
+    mixer.blend(LandscapeGroup, landscape=landscape, group=group, is_default_landscape_group=True)
+    return landscape
