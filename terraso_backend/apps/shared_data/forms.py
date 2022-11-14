47976465a4c5d896b916eaddf79c786a79076538
Jose Buitron <jose.buitron@gmail.com>
2022-11-14 12:02:05 -0500

    Fix: CSV validation (#220)

--- terraso_backend/apps/shared_data/forms.py
+++ terraso_backend/apps/shared_data/forms.py
@@ -14,6 +14,8 @@ from .services import data_entry_upload_service
 mimetypes.init()
 logger = structlog.get_logger(__name__)
 
+VALID_CSV_TYPES = ["text/plain", "text/csv", "application/csv"]
+
 
 class DataEntryForm(forms.ModelForm):
     data_file = forms.FileField()
@@ -37,21 +39,32 @@ class DataEntryForm(forms.ModelForm):
             "created_by",
         )
 
+    def is_valid_mime_type(self, data_file):
+        file_extension = pathlib.Path(data_file.name).suffix
+        content_type = data_file.content_type
+        file_mime_type = magic.from_buffer(data_file.open("rb").read(2048), mime=True)
+        allowed_file_extensions = mimetypes.guess_all_extensions(file_mime_type)
+
+        is_valid = (
+            file_mime_type and allowed_file_extensions and file_extension in allowed_file_extensions
+        )
+
+        # Also check variations of allowed csv types
+        return (
+            is_valid
+            or (
+                file_extension == ".csv"
+                and content_type in VALID_CSV_TYPES
+                and file_mime_type in VALID_CSV_TYPES
+            ),
+            f"Invalid file extension ({file_extension}) for the file type {file_mime_type}",
+        )
+
     def clean_data_file(self):
         data_file = self.cleaned_data["data_file"]
 
-        file_mime_type = magic.from_buffer(data_file.open("rb").read(2048), mime=True)
-        allowed_file_extensions = mimetypes.guess_all_extensions(file_mime_type)
-        file_extension = pathlib.Path(data_file.name).suffix
-
-        if (
-            file_mime_type
-            and allowed_file_extensions
-            and file_extension not in allowed_file_extensions
-        ):
-            message = (
-                f"Invalid file extension ({file_extension}) for the file type {file_mime_type}"
-            )
+        is_valid, message = self.is_valid_mime_type(data_file)
+        if not is_valid:
             logger.info(message)
             raise ValidationError(message, code="invalid_extension")
 
