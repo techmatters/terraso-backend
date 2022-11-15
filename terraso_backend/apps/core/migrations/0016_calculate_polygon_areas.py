b4be4198f5d91940c3384030d0e1e21cfe0993cd
david <david@techmatters.org>
2022-11-15 14:14:48 -0500

    feat: Add migrations for landscape boundary area (#211)

---
+++ terraso_backend/apps/core/migrations/0016_calculate_polygon_areas.py
@@ -0,0 +1,20 @@
+from django.db import migrations, models
+
+from apps.core.geo import calculate_geojson_polygon_area
+
+
+def calculate_area(apps, schema_editor):
+    Landscape = apps.get_model("core", "Landscape")
+    missing_area = Landscape.objects.filter(area_polygon__isnull=False, area_scalar__isnull=True)
+    for landscape in missing_area:
+        landscape.area_scalar = calculate_geojson_polygon_area
+    Landscape.objects.bulk_update(missing_area, ["area_scalar"])
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("core", "0015_landscape_area_scalar"),
+    ]
+
+    operations = [migrations.RunPython(calculate_area)]
