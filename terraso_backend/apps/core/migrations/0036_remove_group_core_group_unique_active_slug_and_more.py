bd19a38b49986c4300eb1aadcebad36da09c51fa
David Code Howard <david@techmatters.org>
2023-01-10 10:38:19 -0500

    fix: Remove unneeded "deleted_at" field alterations from migration

--- terraso_backend/apps/core/migrations/0036_remove_group_core_group_unique_active_slug_and_more.py
+++ terraso_backend/apps/core/migrations/0036_remove_group_core_group_unique_active_slug_and_more.py
@@ -28,58 +28,6 @@ class Migration(migrations.Migration):
             model_name="landscape",
             name="core_landscape_name_key",
         ),
-        migrations.AlterField(
-            model_name="group",
-            name="deleted_at",
-            field=models.DateTimeField(db_index=True, editable=False, null=True),
-        ),
-        migrations.AlterField(
-            model_name="groupassociation",
-            name="deleted_at",
-            field=models.DateTimeField(db_index=True, editable=False, null=True),
-        ),
-        migrations.AlterField(
-            model_name="landscape",
-            name="deleted_at",
-            field=models.DateTimeField(db_index=True, editable=False, null=True),
-        ),
-        migrations.AlterField(
-            model_name="landscapegroup",
-            name="deleted_at",
-            field=models.DateTimeField(db_index=True, editable=False, null=True),
-        ),
-        migrations.AlterField(
-            model_name="membership",
-            name="deleted_at",
-            field=models.DateTimeField(db_index=True, editable=False, null=True),
-        ),
-        migrations.AlterField(
-            model_name="taxonomyterm",
-            name="type",
-            field=models.CharField(
-                choices=[
-                    ("ecosystem-type", "Ecosystem Type"),
-                    ("language", "Language"),
-                    ("livelihood", "Livelihood"),
-                    ("commodity", "Commodity"),
-                    ("organization", "Organization"),
-                    ("agricultural-production-method", "Agricultural Production Method"),
-                ],
-                max_length=128,
-            ),
-        ),
-        migrations.AlterField(
-            model_name="taxonomyterm",
-            name="value_original",
-            field=models.CharField(
-                max_length=128, validators=[apps.core.models.commons.validate_name]
-            ),
-        ),
-        migrations.AlterField(
-            model_name="user",
-            name="deleted_at",
-            field=models.DateTimeField(db_index=True, editable=False, null=True),
-        ),
         migrations.AddConstraint(
             model_name="group",
             constraint=models.UniqueConstraint(
