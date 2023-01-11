3ea7c99c8de77a2b7c90d1dfb440ee8466fd1513
David Code Howard <david@techmatters.org>
2023-01-11 13:57:36 -0500

    fix: Remove unused context class from structlog config (#241)

--- terraso_backend/config/settings.py
+++ terraso_backend/config/settings.py
@@ -182,7 +182,6 @@ structlog.configure(
         structlog.processors.UnicodeDecoder(),
         structlog.stdlib.ProcessorFormatter.wrap_for_formatter,
     ],
-    context_class=structlog.threadlocal.wrap_dict(dict),
     logger_factory=structlog.stdlib.LoggerFactory(),
     wrapper_class=structlog.stdlib.BoundLogger,
     cache_logger_on_first_use=True,
