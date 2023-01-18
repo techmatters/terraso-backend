ec92a7dd0c3ebc330d74c59e751c110ad0b0c9b2
Jose Buitron <jose.buitron@gmail.com>
2023-01-18 13:11:39 -0500

    Update terraso_backend/apps/core/views.py
    Co-authored-by: Paul Schreiber <paul@techmatters.org>

--- terraso_backend/apps/core/views.py
+++ terraso_backend/apps/core/views.py
@@ -61,7 +61,7 @@ class ParseGeoFileView(AuthenticationRequiredMixin, FormView):
                     status=400,
                 )
         else:
-            return JsonResponse({"error": "File type not supported"}, status=400)
+            return JsonResponse({"error": "File type not supported. File type: {file.content_type}"}, status=400)
 
         return JsonResponse({"geojson": geojson})
 
