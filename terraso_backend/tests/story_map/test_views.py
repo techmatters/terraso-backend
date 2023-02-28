19962b6f62af431a3b1ff1a47ef10e35e5ddb573
Jose Buitron <jose.buitron@gmail.com>
2023-02-28 14:57:59 -0500

    chore: Views tests added

---
+++ terraso_backend/tests/story_map/test_views.py
@@ -0,0 +1,128 @@
+import json
+
+import pytest
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.urls import reverse
+from mixer.backend.django import mixer
+
+pytestmark = pytest.mark.django_db
+
+
+def test_add_form_valid(logged_client, users):
+    url = reverse("story_map:add")
+    data = {
+        "title": "Test StoryMap",
+        "configuration": json.dumps(
+            {
+                "title": "Test StoryMap",
+                "chapters": [],
+            }
+        ),
+        "is_published": "true",
+    }
+    response = logged_client.post(url, data=data)
+
+    assert response.status_code == 201
+    assert response.json()["title"] == data["title"]
+    assert response.json()["created_by"] == str(users[0].id)
+    assert response.json()["is_published"]
+
+
+def test_add_form_invalid(logged_client):
+    url = reverse("story_map:add")
+    data = {"title": "Test StoryMap", "is_published": "invalid", "configuration": json.dumps({})}
+    response = logged_client.post(url, data=data)
+
+    assert response.status_code == 400
+    assert len(response.json()["errors"]) == 1
+
+
+def test_update_form_valid(logged_client, users):
+    story_map = mixer.blend("story_map.StoryMap", created_by=users[0])
+    url = reverse("story_map:update")
+    data = {
+        "id": story_map.pk,
+        "title": "Test StoryMap Updated",
+        "configuration": json.dumps({"chapters": []}),
+        "is_published": "false",
+    }
+    response = logged_client.post(url, data=data)
+
+    assert response.status_code == 201
+    assert response.json()["title"] == data["title"]
+    assert not response.json()["is_published"]
+
+
+def test_update_form_invalid(logged_client, users):
+    story_map = mixer.blend("story_map.StoryMap", created_by=users[0])
+    url = reverse("story_map:update")
+    data = {
+        "id": story_map.pk,
+        "title": "",
+        "is_published": "invalid",
+        "configuration": json.dumps(
+            {
+                "title": "Test StoryMap",
+            }
+        ),
+    }
+    response = logged_client.post(url, data=data)
+
+    assert response.status_code == 400
+    assert len(response.json()["errors"]) == 1
+
+
+def test_update_form_unauthorized(logged_client):
+    story_map = mixer.blend("story_map.StoryMap")
+    url = reverse("story_map:update")
+    data = {
+        "id": story_map.pk,
+        "title": "Test StoryMap Updated",
+        "configuration": json.dumps({"chapters": []}),
+        "is_published": "false",
+    }
+    response = logged_client.post(url, data=data)
+
+    assert response.status_code == 400
+    assert len(response.json()["errors"]) == 1
+
+
+def test_update_upload_media(logged_client, users):
+    story_map = mixer.blend("story_map.StoryMap", created_by=users[0])
+    url = reverse("story_map:update")
+    data = {
+        "id": story_map.pk,
+        "title": "Test StoryMap Updated",
+        "is_published": "false",
+        "files": SimpleUploadedFile(
+            name="audio_file.mp3",
+            content="content".encode(),
+            content_type="audio/mp3",
+        ),
+        "configuration": json.dumps(
+            {
+                "title": "Test StoryMap Updated",
+                "chapters": [
+                    {
+                        "id": "chapter-1",
+                        "title": "Chapter 1",
+                        "description": "Chapter 1 description",
+                        "media": {
+                            "contentId": "audio_file.mp3",
+                            "type": "audio/mp3",
+                        },
+                    },
+                ],
+            }
+        ),
+    }
+    response = logged_client.post(url, data=data)
+
+    json_response = response.json()
+
+    print(json_response)
+
+    assert response.status_code == 201
+    assert json_response["configuration"]["chapters"][0]["media"]["url"] is not None
+    assert "story-map-media" in json_response["configuration"]["chapters"][0]["media"]["url"]
+    assert "contentId" not in json_response["configuration"]["chapters"][0]["media"]
