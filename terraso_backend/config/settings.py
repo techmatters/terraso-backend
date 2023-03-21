0a592af45ff5a875c229f198acab8f3af330d05d
Paul Schreiber <paul@techmatters.org>
2023-03-21 16:33:50 -0400

    style: use double quotes in settings

--- terraso_backend/config/settings.py
+++ terraso_backend/config/settings.py
@@ -263,7 +263,7 @@ AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID", default="")
 AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY", default="")
 AWS_S3_REGION_NAME = config("AWS_S3_REGION_NAME", default="us-east-2")
 
-EMAIL_BACKEND = 'django_ses.SESBackend'
+EMAIL_BACKEND = "django_ses.SESBackend"
 AWS_SES_ACCESS_KEY_ID = config("AWS_SES_ACCESS_KEY_ID", default="")
 AWS_SES_SECRET_ACCESS_KEY = config("AWS_SES_SECRET_ACCESS_KEY", default="")
 
