5c3603e9f32e182c458a127f23fc2575d54826b8
Paul Schreiber <paul@techmatters.org>
2023-04-18 17:07:45 -0400

    fix: rename sitemap app to web_client

--- terraso_backend/config/urls.py
+++ terraso_backend/config/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 from django.urls import include, path
 
-from apps.sitemap.sitemap import WebClientSitemap
+from apps.web_client.sitemap import WebClientSitemap
 
 # OAuth2 provider endpoints
 oauth2_endpoint_views = [
