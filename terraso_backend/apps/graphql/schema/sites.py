0b20032da2b57317efcb22e88a4f52e5ba1ff4fb
Jeancarlo <jeancarlobarrios@gmail.com>
2023-05-23 09:14:57 -0600

    feat: add graphql query for sites

--- terraso_backend/apps/graphql/schema/sites.py
+++ terraso_backend/apps/graphql/schema/sites.py
@@ -13,6 +13,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see https://www.gnu.org/licenses/.
 import graphene
+import django_filters
 from graphene import relay
 from graphene_django import DjangoObjectType
 
