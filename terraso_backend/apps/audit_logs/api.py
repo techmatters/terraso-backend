08b2acde9ade8d3acc13162b403652f177441030
Jeancarlo <jeancarlobarrios@gmail.com>
2023-06-07 15:40:47 -0600

    chore: add gitignore at the root level to ignore config files

--- terraso_backend/apps/audit_logs/api.py
+++ terraso_backend/apps/audit_logs/api.py
@@ -30,35 +30,3 @@ class AuditLog(Protocol):
     ) -> None:
         ...
 
-
-class AuditLogQuerier(Protocol):
-    """
-    AuditLogQuerierProtocol is a protocol that defines the interface for the
-    audit log querier
-    """
-
-    def get_logs(self, start: datetime, end: datetime) -> List[models.Log]:
-        ...
-
-    def get_log_by_id(self, log_id: str) -> models.Log:
-        ...
-
-    def get_log_by_key_value(
-        self, values: List[KeyValue], start: datetime, end: datetime
-    ) -> List[models.Log]:
-        ...
-
-    def get_log_by_user(self, user: str, start: datetime, end: datetime) -> List[models.Log]:
-        ...
-
-    def get_log_by_action(self, action: str, start: datetime, end: datetime) -> List[models.Log]:
-        ...
-
-    def get_log_by_resource(self, resource: str) -> List[models.Log]:
-        ...
-
-    def log_to_str(self, logs: models.Log, template: str) -> str:
-        ...
-
-    def logs_to_str(self, logs: List[models.Log], template: str) -> List[str]:
-        ...
