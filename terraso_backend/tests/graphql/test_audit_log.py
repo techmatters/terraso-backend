e317040753f524a1920070ca8449f6587a5a0fc1
Jeancarlo <jeancarlobarrios@gmail.com>
2023-06-07 18:11:53 -0600

    chore: add comments for more readability

--- terraso_backend/tests/graphql/test_audit_log.py
+++ terraso_backend/tests/graphql/test_audit_log.py
@@ -30,11 +30,7 @@ def test_audit_log_query(client_query, audit_log_user, site):
         }
         """,
     )
-    print(response)
     expected_metadata = {
-        "user": audit_log_user.full_name(),
-        "action": "CREATE",
-        "resource": site.human_readable(),
         "some_key": "some_value",
     }
     edges = response.json()["data"]["auditLogs"]["edges"]
@@ -42,9 +38,6 @@ def test_audit_log_query(client_query, audit_log_user, site):
     assert edges[0]["node"]["user"] == {"email": audit_log_user.email}
     assert edges[0]["node"]["resourceId"] == str(site.id)
     assert edges[0]["node"]["metadata"]["some_key"] == expected_metadata["some_key"]
-    assert edges[0]["node"]["metadata"]["user"] == expected_metadata["user"]
-    assert edges[0]["node"]["metadata"]["resource"] == expected_metadata["resource"]
-    assert edges[0]["node"]["metadata"]["action"] == expected_metadata["action"]
 
     logger.log(user=audit_log_user, action=api.CHANGE, resource=site)
     response = client_query(
