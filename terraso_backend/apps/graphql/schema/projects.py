d1b432c3e49fe20d332af2979d9e2375a3ad1643
David Code Howard <david@techmatters.org>
2023-07-18 12:29:51 -0400

    feat: Add fields to Project GraphQL query (#673)
    * feat: Add updated_at field to GraphQL query
    
    This field is needed by the frontend for the project list display.
    
    * feat: Update ProjectNode to include group and site_set fields
    
    Need these fields in order to provide info in the preview on how many
    users belong to a a project, and will also need them to display the
    full results on the mobile client.

--- terraso_backend/apps/graphql/schema/projects.py
+++ terraso_backend/apps/graphql/schema/projects.py
@@ -44,7 +44,7 @@ class ProjectNode(DjangoObjectType):
         model = Project
 
         filterset_class = ProjectFilterSet
-        fields = ("name", "privacy", "description")
+        fields = ("name", "privacy", "description", "updated_at", "group", "site_set")
 
         interfaces = (relay.Node,)
         connection_class = TerrasoConnection
