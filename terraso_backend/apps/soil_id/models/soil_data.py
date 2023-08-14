fe937d89ae25269641165767766e737b09674eb7
jodiezhuu <jodiezhu12@gmail.com>
2023-08-14 13:28:13 -0400

    feat: add slope landscape position, range, and steepness to soil data

--- terraso_backend/apps/soil_id/models/soil_data.py
+++ terraso_backend/apps/soil_id/models/soil_data.py
@@ -26,6 +26,51 @@ LINEAR = "linear"
 
 SLOPE_SHAPES = ((CONCAVE, _("Concave")), (CONVEX, _("Convex")), (LINEAR, _("Linear")))
 
+HILLS_MOUNTAINS = "Hills/Mountains"
+SUMMIT = "Summit"
+SHOULDER = "Shoulder"
+BACKSLOPE = "Backslope"
+ALLUVIAL_FAN = "Alluvial Fan"
+FLOODPLAIN_BASIN = "Floodpain/Basin"
+TERRACE = "Terrace"
+TREAD = "Tread"
+RISER = "Riser"
+FLAT_LOW_ROLLING_PLAIN = "Flat/Low Rolling Plain"
+PLAYA = "Playa"
+DUNES = "Dunes"
+
+LANDSCAPE_POSITIONS = (
+    (
+        HILLS_MOUNTAINS,
+        _((SUMMIT, _("Summit")), (SHOULDER, _("Shoulder")), (BACKSLOPE, _("Backslope"))),
+    ),
+    (ALLUVIAL_FAN, _("Alluvial Fan")),
+    (FLOODPLAIN_BASIN, _("Floodpain/Basin")),
+    (TERRACE, _((TREAD, _("Tread")), (RISER, _("Riser")))),
+    (FLAT_LOW_ROLLING_PLAIN, _("Flat/Low Rolling Plain")),
+    (PLAYA, _("Playa")),
+    (DUNES, _("Dunes")),
+)
+
+FLAT = "0 - 2% (flat)"
+GENTLE = "2 - 5% (gentle)"
+MODERATE = "5 - 10% (moderate)"
+ROLLING = "10 - 15% (rolling)"
+HILLY = "15 - 30% (hilly)"
+STEEP = "30 - 50% (steep)"
+MODERATELY_STEEP = "50 - 60% (moderately steep)"
+VERY_STEEP = "60 - 100% (very steep)"
+STEEPEST = "> 100% (steepest)"
+
+SLOPE_RANGES = ((FLAT, _("0 - 2% (flat)")),
+                (GENTLE, _("2 - 5% (gentle)")),
+                (MODERATE, _("5 - 10% (moderate)")),
+                (ROLLING, _("10 - 15% (rolling)")),
+                (HILLY, _("15 - 30% (hilly)")),
+                (STEEP, _("30 - 50% (steep)")),
+                (MODERATELY_STEEP, _("50 - 60% (moderately steep)")),
+                (VERY_STEEP, _("60 - 100% (very steep)")),
+                (STEEPEST, _("> 100% (steepest)")))
 
 class SoilData(BaseModel):
     site = models.OneToOneField(Site, on_delete=models.CASCADE)
@@ -36,3 +81,9 @@ class SoilData(BaseModel):
         null=True, validators=[MinValueValidator(0), MaxValueValidator(100)]
     )
     slope_shape = models.CharField(null=True, choices=SLOPE_SHAPES)
+    slope_landscape_position = models.CharField(null=True, choices=LANDSCAPE_POSITIONS)
+    slope_aspect = models.IntegerField(null=True, validators=[MinValueValidator(0), MaxValueValidator(359)])
+    slope_range_select = models.CharField(null=True, choices=SLOPE_RANGES)
+    slope_range_manual = models.IntegerField(null=True, validators=[MinValueValidator(0), MaxValueValidator(100)])
+    slope_steepness_percent = models.IntegerField(null=True, validators=[MinValueValidator(0)])
+    slope_steepness_degree = models.IntegerField(null=True, validators=[MinValueValidator(0), MaxValueValidator(90)])
