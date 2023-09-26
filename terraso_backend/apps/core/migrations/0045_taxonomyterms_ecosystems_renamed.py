8a72d68350cea574f2fb732eed0c302a6fcf42fe
Jose Buitron <jose.buitron@gmail.com>
2023-09-26 10:52:56 -0500

    fix: Renamed ecosystems types

---
+++ terraso_backend/apps/core/migrations/0045_taxonomyterms_ecosystems_renamed.py
@@ -0,0 +1,45 @@
+# Copyright © 2023 Technology Matters
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see https://www.gnu.org/licenses/.
+
+from django.db import migrations, models
+
+import apps.core.models.commons
+
+TERMS = {
+    "ecosystem-type": [
+        ("Deserts", "Desert", "Desierto"),
+        ("Savannas", "Savannah", "Sabana"),
+        ("Shrublands", "Shrubland", "Matorrales"),
+    ],
+}
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("core", "0044_notifications"),
+    ]
+
+    operations = [
+        migrations.RunSQL(
+            sql=[
+                (
+                    "UPDATE core_taxonomyterm SET value_es = %s, value_en= %s WHERE value_original = %s AND type= %s",
+                    [value_es, value_en, current_value, type],
+                )
+            ],
+        )
+        for type, values in TERMS.items()
+        for (current_value, value_en, value_es) in values
+    ]
