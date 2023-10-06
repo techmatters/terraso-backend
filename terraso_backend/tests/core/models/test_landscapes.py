c68e9dba36c532fe8fb9defa24ebc3c12fe3f5ac
Jose Buitron <jose.buitron@gmail.com>
2023-10-06 10:55:46 -0500

    fix: Fixed manager test

--- terraso_backend/tests/core/models/test_landscapes.py
+++ terraso_backend/tests/core/models/test_landscapes.py
@@ -20,7 +20,8 @@ import pytest
 from django.core.exceptions import ValidationError
 from mixer.backend.django import mixer
 
-from apps.core.models.groups import Group, Membership
+from apps.core import landscape_collaboration_roles
+from apps.core.models.groups import Group
 from apps.core.models.landscapes import Landscape, LandscapeGroup
 from apps.core.models.users import User
 
@@ -116,10 +117,9 @@ def test_landscape_get_default_group_without_group_returns_none():
 def test_landscape_creator_becomes_manager():
     user = mixer.blend(User)
     landscape = mixer.blend(Landscape, created_by=user)
+    manager_membership = landscape.membership_list.memberships.get(user=user)
 
-    manager_membership = Membership.objects.get(group=landscape.get_default_group(), user=user)
-
-    assert manager_membership.user_role == Membership.ROLE_MANAGER
+    assert manager_membership.user_role == landscape_collaboration_roles.ROLE_MANAGER
 
 
 def test_landscape_area_calculated(unit_polygon):
