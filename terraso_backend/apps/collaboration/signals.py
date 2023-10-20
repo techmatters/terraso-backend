a7694c8dba1fd1fc07d38b7a9212732e2f8cd797
Paul Schreiber <paul@techmatters.org>
2023-10-20 15:10:06 -0400

    fix: case-insensitive email address matching for storymap invites

--- terraso_backend/apps/collaboration/signals.py
+++ terraso_backend/apps/collaboration/signals.py
@@ -23,7 +23,7 @@ from .models.memberships import Membership
 @receiver(user_signup_signal)
 def handle_pending_memberships(sender, **kwargs):
     user = kwargs["user"]
-    pending_memberships = Membership.objects.filter(pending_email=user.email)
+    pending_memberships = Membership.objects.filter(pending_email__iexact=user.email)
     for membership in pending_memberships:
         membership.pending_email = None
         membership.user = user
