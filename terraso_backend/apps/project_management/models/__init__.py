11a584567776cb9f579c509e7481aac4c29f5d26
Aja Beckett <ajabeckett@gmail.com>
2023-10-25 13:11:53 -0700

    chore: add sitenote to init.py

--- terraso_backend/apps/project_management/models/__init__.py
+++ terraso_backend/apps/project_management/models/__init__.py
@@ -1,4 +1,12 @@
 from .projects import Project, ProjectMembership, ProjectMembershipList, ProjectSettings
 from .sites import Site
+from .site_notes import SiteNote
 
-__all__ = ["Project", "ProjectSettings", "Site", "ProjectMembership", "ProjectMembershipList"]
+__all__ = [
+    "Project",
+    "ProjectSettings",
+    "Site",
+    "ProjectMembership",
+    "ProjectMembershipList",
+    "SiteNote"
+]
