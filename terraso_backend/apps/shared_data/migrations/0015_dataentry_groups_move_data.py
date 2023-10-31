356487e68e1c90116856cecfaff84f072101a2c3
Jose Buitron <jose.buitron@gmail.com>
2023-10-31 11:24:22 -0500

    fix: Moved data entries group data migration to shared_data app (#938)
    * fix: Moved data entries group data migration to shared_data app
    
    * fix: Check if groups relation exist

---
+++ terraso_backend/apps/shared_data/migrations/0015_dataentry_groups_move_data.py
@@ -0,0 +1,68 @@
+# Copyright © 2023 Technology Matters
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see https://www.gnu.org/licenses/.
+
+from django.conf import settings
+from django.db import migrations
+
+
+def data_entries_to_shared_resources(apps, schema_editor):
+    connection = schema_editor.connection
+
+    # Check if the relation (table) exists in the database
+    cursor = connection.cursor()
+    cursor.execute("SELECT to_regclass('public.shared_data_dataentry_groups');")
+    relation = cursor.fetchone()
+    if not (relation and relation[0]):
+        return
+
+    ContentType = apps.get_model("contenttypes", "ContentType")
+    LandscapeGroup = apps.get_model("core", "LandscapeGroup")
+    SharedResource = apps.get_model("core", "SharedResource")
+    DataEntry = apps.get_model("shared_data", "DataEntry")
+    data_entries = DataEntry.objects.all()
+    for data_entry in data_entries:
+        groups = data_entry.groups.all()
+        for group in groups:
+            landscape_group = LandscapeGroup.objects.filter(
+                group=group, is_default_landscape_group=True
+            ).first()
+            if landscape_group is None:
+                SharedResource.objects.create(
+                    source_content_type=ContentType.objects.get_for_model(data_entry),
+                    source_object_id=data_entry.id,
+                    target_content_type=ContentType.objects.get_for_model(group),
+                    target_object_id=group.id,
+                )
+            else:
+                SharedResource.objects.create(
+                    source_content_type=ContentType.objects.get_for_model(data_entry),
+                    source_object_id=data_entry.id,
+                    target_content_type=ContentType.objects.get_for_model(
+                        landscape_group.landscape
+                    ),
+                    target_object_id=landscape_group.landscape.id,
+                )
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ("core", "0046_shared_resource"),
+        ("shared_data", "0014_visualizationconfig_owner_null"),
+    ]
+
+    operations = [
+        migrations.RunPython(data_entries_to_shared_resources),
+    ]
