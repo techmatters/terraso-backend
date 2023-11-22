bb1cab6a0802a4cf8adf369f5f58b8a4e41455ab
Jose Buitron <jose.buitron@gmail.com>
2023-11-22 12:58:44 -0500

    fix: Removed old group membership permissions functions

--- terraso_backend/apps/core/permission_rules.py
+++ terraso_backend/apps/core/permission_rules.py
@@ -61,28 +61,6 @@ def allowed_to_delete_landscape_group(user, landscape_group):
 
 
 @rules.predicate
-def allowed_to_change_membership(user, membership_group_id):
-    return user.is_group_manager(membership_group_id)
-
-
-@rules.predicate
-def allowed_to_delete_membership(user, membership_id):
-    from apps.core.models import Membership
-
-    # Users are deleting their own membership
-    if user.memberships.filter(pk=membership_id).exists():
-        return True
-
-    membership = Membership.objects.get(pk=membership_id)
-
-    # Group Managers can delete any Membership in their Groups
-    if user.is_group_manager(membership.group.id):
-        return True
-
-    return False
-
-
-@rules.predicate
 def allowed_group_managers_count(user, membership_id):
     from apps.core.models import Membership
 
