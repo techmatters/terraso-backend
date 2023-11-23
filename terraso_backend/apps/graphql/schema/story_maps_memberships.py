ab324a0d53f592622f3de35ce146f1f6a2bb66c6
Jose Buitron <jose.buitron@gmail.com>
2023-11-23 12:42:37 -0500

    Update terraso_backend/apps/graphql/schema/story_maps_memberships.py
    Co-authored-by: Paul Schreiber <paul@techmatters.org>

--- terraso_backend/apps/graphql/schema/story_maps_memberships.py
+++ terraso_backend/apps/graphql/schema/story_maps_memberships.py
@@ -131,7 +131,7 @@ class StoryMapMembershipApproveTokenMutation(BaseAuthenticatedMutation):
             membership = Membership.objects.get(id=decoded_token["membershipId"])
         except Exception as error:
             logger.error(
-                "Attempt to approve Membership, but it was not found",
+                "Attempt to approve Story Map Membership, but it was not found",
                 extra={"membership_id": decoded_token["membershipId"], "error": error},
             )
             raise GraphQLNotFoundException(model_name=Membership.__name__)
