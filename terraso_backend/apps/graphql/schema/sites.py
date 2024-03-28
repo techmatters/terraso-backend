ffcddb692dca27ccbf8814ef87ae3f44ce141597
Ruxandra Machedon <ruxandra@techmatters.org>
2024-03-28 10:09:33 -0400

    chore: add comment explaining permission check

--- terraso_backend/apps/graphql/schema/sites.py
+++ terraso_backend/apps/graphql/schema/sites.py
@@ -189,6 +189,9 @@ class SiteUpdateMutation(BaseWriteMutation):
         site = cls.get_or_throw(Site, "id", kwargs["id"])
         if not user.has_perm(Site.get_perm("change"), site):
             raise cls.not_allowed(MutationTypes.UPDATE)
+        
+        # if any site settings fields are present in the mutuation, check that the user
+        # has the associated settings-change permission as well
         if Site.SETTINGS_FIELDS.intersection(kwargs.keys()):
             if not user.has_perm(Site.get_perm("change_settings"), site):
                 raise cls.not_allowed(MutationTypes.UPDATE)
