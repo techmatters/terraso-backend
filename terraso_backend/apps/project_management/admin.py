27e08c853262f8ed3629f44828d4d3f6cf80e81d
Paul Schreiber <paul@techmatters.org>
2024-05-02 12:55:16 -0400

    fix: fix linter errors

--- terraso_backend/apps/project_management/admin.py
+++ terraso_backend/apps/project_management/admin.py
@@ -12,7 +12,6 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see https://www.gnu.org/licenses/.
-# from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
