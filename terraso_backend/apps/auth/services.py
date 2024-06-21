edbf143be51a9908bcfbca1d1864fa86016c83de
Paul Schreiber <paul@techmatters.org>
2024-06-21 15:52:13 -0400

    fix: ensure no default value for USER_PREFS_KEY_ACCOUNT_DELETION (#1331)

--- terraso_backend/apps/auth/services.py
+++ terraso_backend/apps/auth/services.py
@@ -30,7 +30,11 @@ from django.utils import timezone
 
 from apps.core.formatters import uppercase_locale
 from apps.core.models import UserPreference
-from apps.core.models.users import USER_PREFS_KEY_LANGUAGE, USER_PREFS_KEYS
+from apps.core.models.users import (
+    USER_PREFS_KEY_ACCOUNT_DELETION,
+    USER_PREFS_KEY_LANGUAGE,
+    USER_PREFS_KEYS,
+)
 from apps.storage.services import ProfileImageService
 
 from .providers import AppleProvider, GoogleProvider, MicrosoftProvider
@@ -87,6 +91,8 @@ class AccountService:
             default_val = "true"
             if notification_key == USER_PREFS_KEY_LANGUAGE:
                 default_val = uppercase_locale(settings.DEFAULT_LANGUAGE_CODE)
+            elif notification_key == USER_PREFS_KEY_ACCOUNT_DELETION:
+                continue
             UserPreference.objects.create(user=user, key=notification_key, value=default_val)
 
     @transaction.atomic
