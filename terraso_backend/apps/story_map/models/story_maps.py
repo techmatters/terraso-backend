0009f28289459637735d57fbdab1ac946eab4846
Jose Buitron <jose.buitron@gmail.com>
2024-11-07 11:33:34 -0500

    feat: (WIP) Story map published version

--- terraso_backend/apps/story_map/models/story_maps.py
+++ terraso_backend/apps/story_map/models/story_maps.py
@@ -30,8 +30,9 @@ class StoryMap(SlugModel):
 
     story_map_id = models.CharField(max_length=10, default="")
     title = models.CharField(max_length=128, validators=[validate_name])
-    configuration = models.JSONField(blank=True, null=True)
     created_by = models.ForeignKey(User, null=True, on_delete=models.DO_NOTHING)
+    configuration = models.JSONField(blank=True, null=True)
+    published_configuration = models.JSONField(blank=True, null=True)
     is_published = models.BooleanField(blank=True, default=False)
     published_at = models.DateTimeField(blank=True, null=True)
 
