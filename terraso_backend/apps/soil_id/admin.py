0bd698bd74365f22ec337cb8d43ae336ab1b567d
Carissa Knipe <carissa@techmatters.org>
2025-10-14 17:11:45 -0700

    feat: SoilMetadata's selected_soil_id now derives from new user_ratings set (#1867)

--- terraso_backend/apps/soil_id/admin.py
+++ terraso_backend/apps/soil_id/admin.py
@@ -21,6 +21,7 @@ from apps.soil_id.models import (
     ProjectSoilSettings,
     SoilData,
     SoilDataDepthInterval,
+    SoilMetadata,
 )
 from apps.soil_id.models.soil_id_cache import SoilIdCache
 
@@ -71,3 +72,26 @@ class SoilDataAdmin(admin.ModelAdmin):
 @admin.register(SoilIdCache)
 class SoilIdCacheAdmin(admin.ModelAdmin):
     list_display = ["id", "latitude", "longitude"]
+
+
+@admin.register(SoilMetadata)
+class SoilMetadataAdmin(admin.ModelAdmin):
+    @admin.display(ordering="site__name")
+    def site_name(self, obj):
+        return obj.site.name
+
+    @admin.display(ordering="site__owner")
+    def site_owner(self, obj):
+        return obj.site.owner
+
+    @admin.display(description="Selected Soil (derived from user ratings)")
+    def selected_soil_display(self, obj):
+        """Display the selected soil ID from user_ratings for backwards compatibility"""
+        return obj.selected_soil_id
+
+    def admin_warning(self, obj):
+        return '^^ WARNING! ^^ \nUser ratings does not enforce proper formatting.\n Format like:\n{"Humic nitisols": "REJECTED", "Haplic nitisols": "SELECTED", "Eutric cambisols": "UNSURE"}'
+
+    list_display = ["site_name", "site_owner", "selected_soil_display"]
+    search_fields = ["site__name", "site__owner__email"]
+    readonly_fields = ["admin_warning", "selected_soil_display"]
