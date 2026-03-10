ee03abe1467010b29ff491880c490bbc4c881d01
Carissa Knipe <carissa@techmatters.org>
2026-03-10 14:04:00 -0700

    test: add unit tests for missing project

--- terraso_backend/tests/graphql/mutations/test_sites.py
+++ terraso_backend/tests/graphql/mutations/test_sites.py
@@ -78,6 +78,52 @@ def test_site_creation_without_elevation(client_query, user):
     assert site.privacy == "public"
 
 
+NONEXISTENT_PROJECT_ID = "00000000-0000-0000-0000-000000000000"
+
+
+# Simulates a case where you create a site in a project that someone deleted since you last pulled (so it's deleted on the server but not in your local client's view)
+def test_site_creation_nonexistent_project_creates_unaffiliated(client_query, user):
+    kwargs = site_creation_keywords()
+    kwargs["projectId"] = NONEXISTENT_PROJECT_ID
+    response = client_query(CREATE_SITE_QUERY, variables={"input": kwargs})
+    content = json.loads(response.content)
+    assert "errors" not in content
+    site_data = content["data"]["addSite"]["site"]
+    assert site_data is not None
+    site = Site.objects.get(pk=site_data["id"])
+    assert site.owner == user
+    assert site.project is None
+
+
+def test_site_creation_viewer_in_project_creates_unaffiliated(client, project, project_user):
+    kwargs = site_creation_keywords()
+    kwargs["projectId"] = str(project.id)
+    client.force_login(project_user)
+    response = graphql_query(CREATE_SITE_QUERY, variables={"input": kwargs}, client=client)
+    content = json.loads(response.content)
+    assert "errors" not in content
+    site_data = content["data"]["addSite"]["site"]
+    assert site_data is not None
+    site = Site.objects.get(pk=site_data["id"])
+    assert site.owner == project_user
+    assert site.project is None
+
+
+def test_site_creation_non_member_of_project_creates_unaffiliated(client, project):
+    non_member = mixer.blend(User)
+    kwargs = site_creation_keywords()
+    kwargs["projectId"] = str(project.id)
+    client.force_login(non_member)
+    response = graphql_query(CREATE_SITE_QUERY, variables={"input": kwargs}, client=client)
+    content = json.loads(response.content)
+    assert "errors" not in content
+    site_data = content["data"]["addSite"]["site"]
+    assert site_data is not None
+    site = Site.objects.get(pk=site_data["id"])
+    assert site.owner == non_member
+    assert site.project is None
+
+
 @pytest.mark.parametrize("project_user_w_role", ["MANAGER", "CONTRIBUTOR"], indirect=True)
 def test_site_creation_in_project(client, project_user_w_role, project):
     kwargs = site_creation_keywords()
